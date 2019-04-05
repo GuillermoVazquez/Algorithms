a1639a2785c7870bf1f7014062c415d469a90fe6
GuillermoVazquez <wvvazque@gmail.com>
2019-04-04 20:13:41 -0400

    update on migratory bird algorithm (will come back to this)

--- migratory_birds.py
+++ migratory_birds.py
@@ -4,16 +4,24 @@ def migratoryBirds(arr):
     index = 0
 
     for bird in arr:
+        bird = int(bird)
         hash[bird-1] += 1
-        if hash[bird-1] > max:
-            max = hash[bird-1]
-            index = bird - 1
-    print(hash)
-    print(index + 1)
-    return index + 1
+        #if hash[bird-1] > max:
+        #    index = bird - 1
+        #    max = hash[bird-1]
+    for x in range(len(hash)):
+        if hash[x] > max:
+            max = hash[x]
+            index = x + 1
+            
+    return index
 
 if __name__ == '__main__':
+    #arr = [1,2,3,3,3,4,4,4]
 
-    arr = [1,1,2,2,3,3]
+    file = open("input04.txt","r")
+    line = file.readline()
+    line = file.readline()
+    arr = line.split(" ")
 
     migratoryBirds(arr)
